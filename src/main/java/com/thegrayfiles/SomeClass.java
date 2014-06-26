f15abaf5cb9551073334572e665da9647fb83c13
Gavin Gray <gavin@vmware.com>
2014-06-25 21:53:22 -0400

    Dummy out some class for javadoc generation.

---
+++ src/main/java/com/thegrayfiles/SomeClass.java
@@ -0,0 +1,27 @@
+package com.thegrayfiles;
+
+import java.math.BigInteger;
+import java.util.Collection;
+
+/**
+ * This is a class that does some amazing stuff.  I don't typically author my files, but I'm doing so here so that it
+ * shows up on the Javadoc generation.
+ *
+ * @author Gavin Gray
+ */
+public class SomeClass {
+
+    /**
+     * Like ths name says, this method does some interesting stuff for us.  Hooray!
+     * <br/>
+     * Although you can't see it, the way I generated that new line between my last thought and this one was by using
+     * <code>&lt;br/&gt;</code>.  In fact, formatting Javadoc is exactly like formatting HTML for text.
+     *
+     * @param aPrimitive a float primitive
+     * @param anObject   an object
+     * @param anArray    a collection of objects
+     */
+    public SomeClass doSomeInterestingStuff(float aPrimitive, Number anObject, Collection<BigInteger> anArray) {
+        return this;
+    }
+}
